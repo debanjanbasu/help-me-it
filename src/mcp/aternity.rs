2da7799cd3ed8a2a6e9c6b4732cb5ad3aa4e0cb5
Debanjan Basu <debanjanbasu2006@gmail.com>
2025-09-29 14:41:00 +1000

    made the basic get tool call work

--- src/mcp/aternity.rs
+++ src/mcp/aternity.rs
@@ -1,7 +1,16 @@
 use axum::http::request;
-use rmcp::{model::{CallToolResult, Content, GetPromptRequestParam, GetPromptResult, ListPromptsResult, PaginatedRequestParam}, tool};
-use anyhow::Result;
-use rmcp::{ErrorData, RoleServer, ServerHandler, handler::server::{router::prompt::PromptRouter, tool::ToolRouter}, model::{Implementation, InitializeRequestParam, InitializeResult, ProtocolVersion, ServerCapabilities, ServerInfo}, prompt_handler, prompt_router, service::RequestContext, tool_handler, tool_router};
+use rmcp::{
+    ErrorData, RoleServer, ServerHandler,
+    handler::server::router::{prompt::PromptRouter, tool::ToolRouter},
+    model::{
+        CallToolResult, Content, GetPromptRequestParam, GetPromptResult, Implementation,
+        InitializeRequestParam, InitializeResult, ListPromptsResult, PaginatedRequestParam,
+        ProtocolVersion, ServerCapabilities, ServerInfo,
+    },
+    prompt_handler, prompt_router,
+    service::RequestContext,
+    tool, tool_handler, tool_router,
+};
 use tracing::info;
 
 #[derive(Clone)]
@@ -21,14 +30,15 @@ impl AternityMCPFactory {
 
     #[tool(description = "Get all Aternity Remediations")]
     async fn get_all_remediations(&self) -> Result<CallToolResult, ErrorData> {
-        Ok(CallToolResult::success(vec![Content::text("Remediation1".to_string())]))
+        Ok(CallToolResult::success(vec![
+            Content::text("Remediation1".to_string()),
+            Content::text("Remediation2".to_string()),
+        ]))
     }
 }
 
 #[prompt_router]
-impl AternityMCPFactory {
-    
-}
+impl AternityMCPFactory {}
 
 #[tool_handler]
 #[prompt_handler]
@@ -36,12 +46,20 @@ impl ServerHandler for AternityMCPFactory {
     fn get_info(&self) -> ServerInfo {
         ServerInfo {
             protocol_version: ProtocolVersion::LATEST,
-            capabilities: ServerCapabilities::builder().enable_prompts().enable_tools().build(),
+            capabilities: ServerCapabilities::builder()
+                .enable_prompts()
+                .enable_tools()
+                .build(),
             server_info: Implementation::from_build_env(),
             instructions: Some("Aternity MCP Server with tools and prompts".to_string()),
         }
     }
-    async fn initialize(&self, _request: InitializeRequestParam, context: RequestContext<RoleServer>) -> Result<InitializeResult, ErrorData> {
+
+    async fn initialize(
+        &self,
+        _request: InitializeRequestParam,
+        context: RequestContext<RoleServer>,
+    ) -> Result<InitializeResult, ErrorData> {
         if let Some(http_request_part) = context.extensions.get::<request::Parts>() {
             let initialize_headers = &http_request_part.headers;
             let initialize_uri = &http_request_part.uri;
